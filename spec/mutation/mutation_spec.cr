fb4a0e346c0941b20ccdf3c3d5f10c46b6a3f0f3
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-10-14 01:20:35 +0200

    Cover a bunch more mutation code

--- spec/mutation/mutation_spec.cr
+++ spec/mutation/mutation_spec.cr
@@ -5,46 +5,97 @@ require "../../src/crytic/mutation/mutation"
 class FakeProcessRunner < Crytic::Mutation::ProcessRunner
   getter cmd
   getter args
+  property exit_code
   @args : String = ""
+
+  def initialize
+    @exit_code = 0
+  end
 
   def run(cmd : String, args : Array(String), output, error)
     @cmd = cmd
     @args = args.join(" ")
-    0
+    @exit_code
   end
 end
 
 module Crytic::Mutation
   describe Mutation do
-    it "evals the mutated code in a separate process" do
-      mutant = Crytic::Mutant::BoolLiteralFlip.new
-      mutation = Mutation.with(
-        mutant,
-        "./fixtures/simple/bar.cr",
-        ["./fixtures/simple/bar_spec.cr"])
+    describe "#run" do
+      it "evals the mutated code in a separate process" do
+        mutant = Crytic::Mutant::BoolLiteralFlip.new
+        mutation = Mutation.with(
+          mutant,
+          "./fixtures/simple/bar.cr",
+          ["./fixtures/simple/bar_spec.cr"])
 
-      fake = FakeProcessRunner.new
-      mutation.process_runner = fake
+        fake = FakeProcessRunner.new
+        mutation.process_runner = fake
 
-      mutation.run.should be_a(Crytic::Mutation::Result)
-      fake.cmd.should eq "crystal"
-      fake.args.should eq <<-CODE
-      eval \ndef bar
-        if false
-          2
-        else
-          3
+        mutation.run.should be_a(Crytic::Mutation::Result)
+        fake.cmd.should eq "crystal"
+        fake.args.should eq <<-CODE
+        eval \ndef bar
+          if false
+            2
+          else
+            3
+          end
         end
+
+        require "spec"
+        describe("bar") do
+          it("works") do
+            bar.should(eq(2))
+          end
+        end
+
+        CODE
       end
 
-      require "spec"
-      describe("bar") do
-        it("works") do
-          bar.should(eq(2))
-        end
+      it "considers a mutant covered if the process fails" do
+        mutant = Crytic::Mutant::BoolLiteralFlip.new
+        mutation = Mutation.with(
+          mutant,
+          "./fixtures/simple/bar.cr",
+          ["./fixtures/simple/bar_spec.cr"])
+
+        fake = FakeProcessRunner.new
+        fake.exit_code = 1
+        mutation.process_runner = fake
+
+        mutation.run.is_covered.should eq true
       end
 
-      CODE
+      it "considers a mutant uncovered if the process succeeds" do
+        mutant = Crytic::Mutant::BoolLiteralFlip.new
+        mutation = Mutation.with(
+          mutant,
+          "./fixtures/simple/bar.cr",
+          ["./fixtures/simple/bar_spec.cr"])
+
+        fake = FakeProcessRunner.new
+        fake.exit_code = 0
+        mutation.process_runner = fake
+
+        mutation.run.is_covered.should eq false
+      end
+
+      it "returns a colored diff of the changes" do
+        mutant = Crytic::Mutant::BoolLiteralFlip.new
+        mutation = Mutation.with(
+          mutant,
+          "./fixtures/simple/bar.cr",
+          ["./fixtures/simple/bar_spec.cr"])
+
+        fake = FakeProcessRunner.new
+        fake.exit_code = 0
+        mutation.process_runner = fake
+
+        mutation.run.diff.should eq <<-DIFF
+        \e[90mdef bar\n  if \e[0m\n\e[31mtru\e[0m\n\e[32mfals\e[0m\n\e[90me\n    2\n  else\n    3\n  end\nend\e[0m\n\e[31m\n\e[0m\n
+        DIFF
+      end
     end
   end
 end
