8887673b00e36fe6dd25f047a93f7b52ba4245dc
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-10-13 23:18:31 +0200

    Extract io reporter for cli output

---
+++ src/crytic/io_reporter.cr
@@ -0,0 +1,29 @@
+require "./mutation/mutation"
+
+module Crytic
+  class IoReporter
+    INDENT = "    "
+
+    def initialize(@io : IO)
+    end
+
+    def report(original_result, results)
+      @io << "Original suite: "
+      @io << "#{original_result.exit_code == 0 ? "✅" : "❌"}\n"
+      @io << "Mutations covered by tests:\n\n"
+      results.map do |result|
+        @io << INDENT
+        @io << (result.is_covered ? "✅ #{result.mutant_name}" : "❌ #{result.mutant_name}")
+
+        unless result.is_covered
+          @io << "\n#{INDENT + INDENT}The following change didn't fail the test-suite:\n"
+          @io << "#{INDENT + INDENT + INDENT}"
+          @io << result.diff.lines.join("\n#{INDENT + INDENT + INDENT}")
+          @io << "\n"
+        end
+
+        @io << "\n"
+      end
+    end
+  end
+end
--- src/crytic/runner.cr
+++ src/crytic/runner.cr
@@ -1,11 +1,11 @@
-require "./source"
+require "./io_reporter"
 require "./mutant/**"
 require "./mutation/mutation"
 require "./mutation/no_mutation"
+require "./source"
 
 module Crytic
   class Runner
-    INDENT = "    "
     MUTANTS = [
       Mutant::ConditionFlip.new,
       Mutant::NumberLiteralChange.new,
@@ -25,22 +25,7 @@ module Crytic
         Mutation.with(mutant: mutant, original: source, specs: specs).run
       end.select(&.applicable)
 
-      @io << "Original suite: "
-      @io << "#{original_result.exit_code == 0 ? "✅" : "❌"}\n"
-      @io << "Mutations covered by tests:\n\n"
-      results.map do |result|
-        @io << INDENT
-        @io << (result.is_covered ? "✅ #{result.mutant_name}" : "❌ #{result.mutant_name}")
-
-        unless result.is_covered
-          @io << "\n#{INDENT + INDENT}The following change didn't fail the test-suite:\n"
-          @io << "#{INDENT + INDENT + INDENT}"
-          @io << result.diff.lines.join("\n#{INDENT + INDENT + INDENT}")
-          @io << "\n"
-        end
-
-        @io << "\n"
-      end
+      IoReporter.new(@io).report(original_result, results)
 
       return results.map(&.is_covered).all?
     end
