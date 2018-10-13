6199e5de6b1d8256f941059326e5e4bf9e080ee6
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-10-14 01:14:04 +0200

    Cover some mutation code

--- spec/mutation/mutation_spec.cr
+++ spec/mutation/mutation_spec.cr
@@ -1,9 +1,50 @@
 require "spec"
+require "../../src/crytic/mutant/bool_literal_flip"
 require "../../src/crytic/mutation/mutation"
 
-module Crytic
+class FakeProcessRunner < Crytic::Mutation::ProcessRunner
+  getter cmd
+  getter args
+  @args : String = ""
+
+  def run(cmd : String, args : Array(String), output, error)
+    @cmd = cmd
+    @args = args.join(" ")
+    0
+  end
+end
+
+module Crytic::Mutation
   describe Mutation do
-    it "works" do
+    it "evals the mutated code in a separate process" do
+      mutant = Crytic::Mutant::BoolLiteralFlip.new
+      mutation = Mutation.with(
+        mutant,
+        "./fixtures/simple/bar.cr",
+        ["./fixtures/simple/bar_spec.cr"])
+
+      fake = FakeProcessRunner.new
+      mutation.process_runner = fake
+
+      mutation.run.should be_a(Crytic::Mutation::Result)
+      fake.cmd.should eq "crystal"
+      fake.args.should eq <<-CODE
+      eval \ndef bar
+        if false
+          2
+        else
+          3
+        end
+      end
+
+      require "spec"
+      describe("bar") do
+        it("works") do
+          bar.should(eq(2))
+        end
+      end
+
+      CODE
     end
   end
 end
--- src/crytic/mutation/mutation.cr
+++ src/crytic/mutation/mutation.cr
@@ -2,14 +2,29 @@ require "../mutant/mutant"
 require "./diff"
 require "./inject_mutated_subject_into_specs"
 require "./result"
+require "../source"
 
 module Crytic::Mutation
+  abstract class ProcessRunner
+    abstract def run(cmd : String, args, output, error)
+  end
+
+  class ProcessProcessRunner < ProcessRunner
+    def run(cmd, args, output, error)
+      Process.run(cmd, args, output: output, error: error).exit_code
+    end
+  end
+
   class Mutation
+    property process_runner
+    @process_runner : ProcessRunner
+
     private def initialize(
       @mutant : Crytic::Mutant::Mutant,
       @subject_file_path : String,
       @specs_file_paths : Array(String))
       @io = IO::Memory.new
+      @process_runner = ProcessProcessRunner.new
     end
 
     def run
@@ -21,20 +36,29 @@ module Crytic::Mutation
         return Result.new(is_covered: false, mutant: @mutant, diff: source_diff)
       end
 
-      mutated_specs_source = @specs_file_paths.map do |spec_file|
-        spec_code = Crystal::Parser.parse(File.read(spec_file))
-        spec_code.accept(InjectMutatedSubjectIntoSpecs.new(@subject_file_path, spec_file))
-        spec_code.to_s
-      end.join("\n").gsub(/require "PUTMEHERE"/, "\n#{mutated_source}\n")
-
-      # puts mutated_specs_source
-
-      res = Process.run("crystal", ["eval", mutated_specs_source], output: @io, error: STDERR)
-      Result.new(is_covered: res.exit_code != 0, mutant: @mutant, diff: source_diff)
+      Result.new(
+        is_covered: run_process(mutated_source) != 0,
+        mutant: @mutant,
+        diff: source_diff)
     end
 
     def self.with(mutant : Mutant::Mutant, original : String, specs : Array(String) )
       new(mutant, original, specs)
+    end
+
+    private def run_process(mutated_source)
+      process_runner.run(
+        "crystal", ["eval", mutated_specs_source(mutated_source)],
+        output: @io,
+        error: STDERR)
+    end
+
+    private def mutated_specs_source(mutated_source)
+      @specs_file_paths.map do |spec_file|
+        spec_code = Crystal::Parser.parse(File.read(spec_file))
+        spec_code.accept(InjectMutatedSubjectIntoSpecs.new(@subject_file_path, spec_file))
+        spec_code.to_s
+      end.join("\n").gsub(/require "PUTMEHERE"/, "\n#{mutated_source}\n")
     end
   end
 end
