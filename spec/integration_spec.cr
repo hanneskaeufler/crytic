9ddb58fbacaa8aabb27f0341354c8aa80325d50b
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-10-14 02:04:36 +0200

    Proper crytic command with integration specs

--- spec/integration_spec.cr
+++ spec/integration_spec.cr
@@ -1,48 +1,44 @@
 require "spec"
 require "../src/crytic/runner"
 
-describe Crytic::Runner do
+describe Crytic do
   describe "with a fully covered subject" do
     it "passes the mutation specs" do
-      io = IO::Memory.new
-      Crytic::Runner.new(io).run(
-        "./fixtures/simple/bar.cr",
-        [
-          "./fixtures/simple/bar_spec.cr"
-        ]
-      ).should eq true
-      io.to_s.should contain("✅ ConditionFlip")
-      io.to_s.should contain("✅ BoolLiteralFlip")
+      result = run_crytic("-s ./fixtures/simple/bar.cr ./fixtures/simple/bar_spec.cr")
+      result.exit_code.should eq 0
+      result.output.should contain("✅ ConditionFlip")
+      result.output.to_s.should contain("✅ BoolLiteralFlip")
     end
   end
 
   describe "with an insufficiently covered subject" do
     it "fails the mutation specs" do
-      io = IO::Memory.new
-      Crytic::Runner.new(io).run(
-        "./fixtures/conditionals/fully_covered.cr",
-        [
-          "./fixtures/conditionals/uncovered_spec.cr"
-        ]
-      ).should eq false
-      io.to_s.should contain("❌ BoolLiteralFlip")
-      io.to_s.should contain("❌ ConditionFlip")
+      result = run_crytic("-s ./fixtures/conditionals/fully_covered.cr ./fixtures/conditionals/uncovered_spec.cr")
+      result.exit_code.should be > 0
+      result.output.should contain("❌ BoolLiteralFlip")
+      result.output.should contain("❌ ConditionFlip")
     end
   end
 
   describe "subject without any coverage" do
     it "fails all mutants" do
-      io = IO::Memory.new
-      Crytic::Runner.new(io).run(
-        "./fixtures/uncovered/without.cr",
-        [
-          "./fixtures/uncovered/without_spec.cr"
-        ]
-      ).should eq false
-      io.to_s.should contain("❌ BoolLiteralFlip")
-      io.to_s.should contain("❌ ConditionFlip")
-      io.to_s.should contain("❌ NumberLiteralSignFlip")
-      io.to_s.should contain("❌ NumberLiteralChange")
+      result = run_crytic("-s ./fixtures/uncovered/without.cr ./fixtures/uncovered/without_spec.cr")
+      result.exit_code.should be > 0
+      result.output.should contain("❌ BoolLiteralFlip")
+      result.output.should contain("❌ ConditionFlip")
+      result.output.should contain("❌ NumberLiteralSignFlip")
+      result.output.should contain("❌ NumberLiteralChange")
     end
   end
 end
+
+def run_crytic(args : String)
+  io = IO::Memory.new
+  result = Process.run("crystal run src/crytic.cr -- #{args}",
+                       output: io,
+                       error: io,
+                       shell: true)
+  CryticResult.new(exit_code: result.exit_code, output: io.to_s)
+end
+
+record CryticResult, exit_code : Int32, output : String
--- src/crytic.cr
+++ src/crytic.cr
@@ -1,20 +1,27 @@
+require "option_parser"
 require "./crytic/runner"
 
+subject_source = ""
+spec_files = [] of String
+
+OptionParser.parse! do |parser|
+  parser.banner = "Usage: crytic [arguments]"
+  parser.on("-s SOURCE", "--subject=SOURCE", "Specifies the source file for the subject") do|source|
+    subject_source = source
+  end
+  parser.on("-h", "--help", "Show this help") { puts parser }
+  parser.unknown_args do |args|
+    spec_files = args
+  end
+  parser.invalid_option do |flag|
+    STDERR.puts "ERROR: #{flag} is not a valid option."
+    STDERR.puts parser
+    exit(1)
+  end
+end
+
 io = IO::Memory.new
-success = Crytic::Runner.new(io).run(
-        "./fixtures/uncovered/without.cr",
-        [
-          "./fixtures/uncovered/without_spec.cr"
-        ]
-        # "./fixtures/conditionals/fully_covered.cr",
-        # [
-        #   "./fixtures/conditionals/uncovered_spec.cr"
-        # ]
-  # "./fixtures/simple/bar.cr",
-  # [
-  #   "./fixtures/simple/bar_spec.cr"
-  # ]
-)
+success = Crytic::Runner.new(io).run(subject_source, spec_files)
 puts io.to_s
 
 exit(success ? 0 : 1)
