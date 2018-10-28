db3a029f7742567f633ef362a0548a41bac2e0a2
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-10-28 19:01:12 +0100

    Save mutated code into tempfile, compile and run that
    `crystal eval` forks and actually runs the piped code in a tempfile.
    But when running crystal eval in a process, that means I cannot kill
    that forked process. Instead I can save the code to a tempfile, compile
    and run that, which lets me actually kill the running process.

--- spec/mutation/fake_process_runner.cr
+++ spec/mutation/fake_process_runner.cr
@@ -2,19 +2,24 @@ require "../../src/crytic/process_runner"
 
 module Crytic
   class FakeProcessRunner < ProcessRunner
-    getter cmd
-    getter args
+    private getter cmd
+    private getter args
     property exit_code
-    @args : String = ""
+    @cmd = [] of String
+    @args = [] of String
     @output_io = IO::Memory.new
+
+    def cmd_with_args
+      cmd.zip(args).map { |c, a| "#{c} #{a}".strip }
+    end
 
     def initialize
       @exit_code = 0
     end
 
     def run(cmd : String, args : Array(String), output, error)
-      @cmd = cmd
-      @args = args.join(" ")
+      @cmd << cmd
+      @args << args.join(" ")
       output << @output_io.to_s
       @exit_code
     end
