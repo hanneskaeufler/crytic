4147f085ff5819764750fdb9ff0602cd506526c6
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-10-28 19:21:12 +0100

    Use latest fake improvements

--- src/crytic/process_process_runner.cr
+++ src/crytic/process_process_runner.cr
@@ -2,11 +2,15 @@ require "./process_runner"
 require "timeout"
 
 module Crytic
+  # Runs processes using crystals `Process` class
   class ProcessProcessRunner < ProcessRunner
+    # Run the given command with args and save output to given io
     def run(cmd, args, output, error) : Int32
       Process.run(cmd, args, output: output, error: error).exit_code
     end
 
+    # Run the given command with args and save output to given io
+    # Times out and kills the provess after given period.
     def run(cmd, args, output, error, timeout) : Int32
       channel = Channel(Int32).new
       process = Process.new(cmd, args, output: output, error: error)
