5d7788f03a1d2cbf35198fcf90873be6a3fb267a
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-12-11 23:26:25 +0100

    Cannot use __DIR__ because of crytics limitations

--- spec/generator/in_memory_generator_spec.cr
+++ spec/generator/in_memory_generator_spec.cr
@@ -2,11 +2,16 @@ require "../../src/crytic/generator/in_memory_generator"
 require "../spec_helper"
 require "compiler/crystal/syntax/*"
 
+# Gotta have this here becaus __DIR__ evaluates at compile time and therefore means
+# that when we run crytic on this codebase itself, it will fail because __DIR__ would
+# evaluate to "." which is not the correct path. See https://github.com/hanneskaeufler/crytic/issues/19
+DIR = "./spec/generator"
+
 module Crytic
   describe InMemoryMutationsGenerator do
     describe "#mutations_for" do
       it "returns no mutations for no possibilities" do
-        source = "#{__DIR__}/non_empty_source_file.cr"
+        source = "#{DIR}/non_empty_source_file.cr"
         specs = ["some_spec.cr"]
 
         mutations = InMemoryMutationsGenerator
@@ -17,7 +22,7 @@ module Crytic
       end
 
       it "returns no mutations for no possibilities in the source" do
-        source = "#{__DIR__}/empty_source_file.cr"
+        source = "#{DIR}/empty_source_file.cr"
         specs = ["some_spec.cr"]
 
         mutations = InMemoryMutationsGenerator.new.mutations_for(source, specs)
@@ -26,7 +31,7 @@ module Crytic
       end
 
       it "returns a single mutation for the number literal" do
-        source = "#{__DIR__}/non_empty_source_file.cr"
+        source = "#{DIR}/non_empty_source_file.cr"
         specs = ["some_spec.cr"]
 
         mutations = InMemoryMutationsGenerator
@@ -37,7 +42,7 @@ module Crytic
       end
 
       it "doesn't mix mutations for multiple sources" do
-        source = "#{__DIR__}/non_empty_source_file.cr"
+        source = "#{DIR}/non_empty_source_file.cr"
         specs = ["some_spec.cr"]
 
         generator = InMemoryMutationsGenerator
