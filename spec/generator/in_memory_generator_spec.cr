59d1af1d6853116cb5aa275611f2845fa993bbf4
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-01-08 19:09:51 +0100

    Cleaner specs through helpers

--- spec/generator/in_memory_generator_spec.cr
+++ spec/generator/in_memory_generator_spec.cr
@@ -2,17 +2,11 @@ require "../../src/crytic/generator/in_memory_generator"
 require "../spec_helper"
 require "compiler/crystal/syntax/*"
 
-# Gotta have this here becaus __DIR__ evaluates at compile time and therefore means
-# that when we run crytic on this codebase itself, it will fail because __DIR__ would
-# evaluate to "." which is not the correct path. See https://github.com/hanneskaeufler/crytic/issues/19
-DIR = "./spec/generator"
-
 module Crytic
   describe InMemoryMutationsGenerator do
     describe "#mutations_for" do
       it "returns no mutations for no possibilities" do
-        source = ["#{DIR}/non_empty_source_file.cr"]
-        specs = ["some_spec.cr"]
+        source = fixture_source("non_empty_source_file.cr")
 
         mutations = InMemoryMutationsGenerator
           .new([] of Mutant::Possibilities)
@@ -22,8 +16,7 @@ module Crytic
       end
 
       it "returns no mutations for no possibilities in the source" do
-        source = ["#{DIR}/empty_source_file.cr"]
-        specs = ["some_spec.cr"]
+        source = fixture_source("empty_source_file.cr")
 
         mutations = InMemoryMutationsGenerator.new.mutations_for(source, specs)
 
@@ -31,8 +24,7 @@ module Crytic
       end
 
       it "returns a single mutation for the number literal" do
-        source = ["#{DIR}/non_empty_source_file.cr"]
-        specs = ["some_spec.cr"]
+        source = fixture_source("non_empty_source_file.cr")
 
         mutations = InMemoryMutationsGenerator
           .new([Mutant::NumberLiteralSignFlipPossibilities.new] of Mutant::Possibilities)
@@ -42,8 +34,7 @@ module Crytic
       end
 
       it "doesn't mix mutations for multiple sources" do
-        source = ["#{DIR}/non_empty_source_file.cr"]
-        specs = ["some_spec.cr"]
+        source = fixture_source("non_empty_source_file.cr")
 
         generator = InMemoryMutationsGenerator
           .new([Mutant::NumberLiteralSignFlipPossibilities.new] of Mutant::Possibilities)
@@ -55,4 +46,17 @@ module Crytic
       end
     end
   end
+end
+
+# Gotta have this here becaus __DIR__ evaluates at compile time and therefore means
+# that when we run crytic on this codebase itself, it will fail because __DIR__ would
+# evaluate to "." which is not the correct path. See https://github.com/hanneskaeufler/crytic/issues/19
+DIR = "./spec/generator"
+
+private def fixture_source(filename)
+  ["#{DIR}/#{filename}"]
+end
+
+private def specs
+  ["some_spec.cr"]
 end
