e22a8f3ef24dbd8bf2d04b3e767aefd975013796
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-01-20 19:05:29 +0100

    Use grouping key

--- src/crytic/reporter/file_summary_io_reporter.cr
+++ src/crytic/reporter/file_summary_io_reporter.cr
@@ -8,7 +8,7 @@ module Crytic::Reporter
       results
         .group_by(&.mutated_file)
         .each do |filename, results|
-          @io.puts "| #{results.first.mutated_file} | #{results.size} |"
+          @io.puts "| #{filename} | #{results.size} |"
         end
       @io.puts "-----------"
     end
