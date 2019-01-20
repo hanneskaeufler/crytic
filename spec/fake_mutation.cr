3dfeca137abb2e6c82db20cef082cc1067de8cd8
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-01-20 22:01:51 +0100

    Fix problems after rebasing

--- spec/fake_mutation.cr
+++ spec/fake_mutation.cr
@@ -1,20 +1,13 @@
 require "../../src/crytic/mutant/bool_literal_flip"
 require "../../src/crytic/mutation/mutation"
 require "../../src/crytic/mutation/result"
-
-private def irrelevant_mutant
-  Crytic::Mutant::BoolLiteralFlip.at(Crystal::Location.new(
-    filename: nil,
-    line_number: 2,
-    column_number: 6,
-  ))
-end
+require "./spec_helper"
 
 class FakeMutation
   property run_call_count = 0
 
   def run
     @run_call_count += 1
-    Crytic::Mutation::Result.new(Crytic::Mutation::Status::Uncovered, irrelevant_mutant, "")
+    Crytic::Mutation::Result.new(Crytic::Mutation::Status::Uncovered, fake_mutant, "")
   end
 end
