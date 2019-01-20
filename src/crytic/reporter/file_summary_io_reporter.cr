0c0c8c9120c9a41da79b921be5693607f8dcd133
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-01-20 19:56:24 +0100

    Extract methods

--- src/crytic/reporter/file_summary_io_reporter.cr
+++ src/crytic/reporter/file_summary_io_reporter.cr
@@ -17,24 +17,42 @@ module Crytic::Reporter
     end
 
     def report_summary(results)
-      width = results.map(&.mutated_file.size).max? || 0
-      header = header(width)
-
-      @io.puts header
-      @io.puts "-".rjust(header.size, '-')
+      longest_width = longest_filename_width(results)
+      total_width = header(longest_width)
 
       results
         .group_by(&.mutated_file)
         .each do |filename, results|
-        @io.puts "| #{filename.ljust(width)} | #{results.size.to_s.rjust(MUTANTS.size)} |"
+          @io.puts "| #{filename.ljust(longest_width)} | #{results.size.to_s.rjust(MUTANTS.size)} |"
         end
-      @io.puts "-".rjust(header.size, '-')
+
+      footer(total_width)
     end
 
     def report_msi(results)
     end
 
-    private def header(width)
+    private def longest_filename_width(results)
+      results.map(&.mutated_file.size).max? || 0
+    end
+
+    private def header(longest_filename_width)
+      column_names = column_names(longest_filename_width)
+      # @io.puts ruler(column_names.size)
+      @io.puts column_names
+      @io.puts ruler(column_names.size)
+      column_names.size
+    end
+
+    private def footer(width)
+      @io.puts ruler(width)
+    end
+
+    private def ruler(width)
+      "-".rjust(width, '-')
+    end
+
+    private def column_names(width)
       "|#{" File ".ljust(width + 2)}| #{MUTANTS} |"
     end
   end
