bc285ba95be3d8ab559b190f7996e64218e0e919
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-02-12 18:51:51 +0100

    Handle unknown options

--- spec/cli_options_spec.cr
+++ spec/cli_options_spec.cr
@@ -9,7 +9,7 @@ module Crytic
     @spec_files = [] of String
     @subject = [] of String
 
-    def initialize(@std_out : IO, @exit_fun : (Int32)->)
+    def initialize(@std_out : IO, @std_err : IO,  @exit_fun : (Int32)->)
     end
 
     def parse(args)
@@ -27,6 +27,12 @@ module Crytic
 
         parser.unknown_args do |args|
           @spec_files = args
+        end
+
+        parser.invalid_option do |flag|
+          @std_err.puts "ERROR: #{flag} is not a valid option."
+          @std_err.puts parser
+          @exit_fun.call(1)
         end
       end
 
@@ -50,11 +56,23 @@ end
 module Crytic
   describe CliOptions do
     describe "#parse" do
+      it "fails early for unknown flags" do
+        std_err = IO::Memory.new
+        exit_code : Int32? = nil
+
+        CliOptions
+          .new(IO::Memory.new, std_err, ->(code : Int32) { exit_code = code })
+          .parse(["-unknown"])
+
+        std_err.to_s.lines.first.should eq "ERROR: -unknown is not a valid option."
+        exit_code.should eq 1
+      end
+
       it "shows help for -h" do
         std_out = IO::Memory.new
 
         CliOptions
-          .new(std_out, noop_exit_fun)
+          .new(std_out, IO::Memory.new, noop_exit_fun)
           .parse(["-h"])
 
         std_out.to_s.lines.first.should eq "Usage: crytic [arguments]"
@@ -65,7 +83,7 @@ module Crytic
         std_out = IO::Memory.new
 
         CliOptions
-          .new(std_out, noop_exit_fun)
+          .new(std_out, IO::Memory.new, noop_exit_fun)
           .parse([{{ flag }}])
 
         std_out.to_s.lines.first.should eq "Usage: crytic [arguments]"
@@ -77,7 +95,7 @@ module Crytic
         exit_code : Int32? = nil
 
         CliOptions
-          .new(IO::Memory.new, ->(code : Int32){ exit_code = code })
+          .new(IO::Memory.new, IO::Memory.new, ->(code : Int32){ exit_code = code })
           .parse(["--help"])
 
         exit_code.should eq 0
@@ -85,7 +103,7 @@ module Crytic
 
       it "adds every positional argument as a spec file" do
         opts = CliOptions
-          .new(IO::Memory.new, noop_exit_fun)
+          .new(IO::Memory.new, IO::Memory.new, noop_exit_fun)
           .parse(["a_file.cr", "another_file.cr"])
 
         opts.spec_files.should eq ["a_file.cr", "another_file.cr"]
@@ -93,7 +111,7 @@ module Crytic
 
       it "defaults to a glob in spec for the spec files" do
         opts = CliOptions
-          .new(IO::Memory.new, noop_exit_fun)
+          .new(IO::Memory.new, IO::Memory.new, noop_exit_fun)
           .parse([] of String)
 
         opts.spec_files.should eq Dir["./spec/**/*_spec.cr"]
@@ -102,7 +120,7 @@ module Crytic
       {% for flag in ["-s", "--subject"] %}
       it "accepts a subject with {{ flag.id }}" do
         opts = CliOptions
-          .new(IO::Memory.new, noop_exit_fun)
+          .new(IO::Memory.new, IO::Memory.new, noop_exit_fun)
           .parse([{{ flag }}, "subject.cr"])
 
         opts.subject.should eq ["subject.cr"]
@@ -111,7 +129,7 @@ module Crytic
 
       it "defaults to a glob in src for the subject" do
         opts = CliOptions
-          .new(IO::Memory.new, noop_exit_fun)
+          .new(IO::Memory.new, IO::Memory.new, noop_exit_fun)
           .parse([] of String)
 
         opts.subject.should eq Dir["./src/**/*.cr"]
