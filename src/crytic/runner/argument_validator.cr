16c5028cf90583990c90679cdf728bb22578a36c
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-03-10 12:37:17 +0100

    Remove unused argument

---
+++ src/crytic/runner/argument_validator.cr
@@ -0,0 +1,11 @@
+module Crytic::Runner
+  module ArgumentValidator
+    private def validate_args!(specs)
+      specs.each do |spec_file|
+        unless File.exists?(spec_file)
+          raise ArgumentError.new("Spec file #{spec_file} doesn't exist.")
+        end
+      end
+    end
+  end
+end
