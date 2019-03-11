05214e530ce18d4a59aac442d1b6c332b7c2a226
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-03-11 22:16:10 +0100

    Back to inlined instantiation

--- src/crytic/cli.cr
+++ src/crytic/cli.cr
@@ -11,18 +11,14 @@ module Crytic
       args = args.tap(&.shift) if args.first? == "test"
       case args.first?
       when "test"
-        test_command.execute(args.tap(&.shift))
+        Command::Test.new(@side_effects).execute(args.tap(&.shift))
       when "noop"
         Command::Noop
-          .new(@std_out, @std_err, Command::Noop::DEFAULT_SPEC_FILES_GLOB)
+          .new(@side_effects, Command::Noop::DEFAULT_SPEC_FILES_GLOB)
           .execute(args.tap(&.shift))
       else
-        test_command.execute(args)
+        Command::Test.new(@side_effects).execute(args)
       end
-    end
-
-    private def test_command
-      Command::Test.new(@side_effects.std_out, @side_effects.std_err, @side_effects.exit_fun, @side_effects.env)
     end
   end
 end
